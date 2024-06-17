145c90a43c5d008c24ffa80cec08da34481038f6
Ashwinujjwalbharti <135020791+Ashwinujjwalbharti@users.noreply.github.com>
2024-06-17 19:02:19 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,21 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCqyhjAzmwTzSoPMk8NCkSOps_Gu7eM7_Y",
-//   authDomain: "netflixclone-af56a.firebaseapp.com",
-//   projectId: "netflixclone-af56a",
-//   storageBucket: "netflixclone-af56a.appspot.com",
-//   messagingSenderId: "1077389074595",
-//   appId: "1:1077389074595:web:4aed570b4f13d8629e9f16",
-//   measurementId: "G-YV9VR6HZSR",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// // to initialize auth and database
-// export const auth = getAuth(app);
-// export const provider = new GoogleAuthProvider();
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBav-zU4KNPVGWNWsvXqWmHDdyzDrYP4BY",
